ea6a46c173158bac2fc6b9bd04b5b94c7b041a42
Sajed Lopez Mendoza <140778693+qppd@users.noreply.github.com>
2026-03-13 12:04:12 +0800

    Refactor LimitSwitch.h header guard and comments
    Updated header guard from COMPONENTS_LIMIT_SWITCH_H to COMPONENT_LIMIT_SWITCH_H and removed usage comments.

--- source/client/SmartCabinet/LimitSwitch.h
+++ source/client/SmartCabinet/LimitSwitch.h
@@ -3,11 +3,6 @@
 
 #include <Arduino.h>
 
-// Simple debounced limit switch helper for ESP32
-// Usage:
-//  LimitSwitch sw(pin, activeLow=true, debounceMs=50);
-//  sw.begin(); sw.update(); if (sw.isPressed()) ...
-
 class LimitSwitch {
 public:
   LimitSwitch(uint8_t pin, bool activeLow = true, unsigned long debounceMs = 50);
@@ -28,4 +23,4 @@ private:
   Callback _cb;
 };
 
-#endif // COMPONENTS_LIMIT_SWITCH_H
+#endif // COMPONENT_LIMIT_SWITCH_H
